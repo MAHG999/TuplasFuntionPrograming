68eed20048ec9ad9666b23040e527dd0a7d53bd6
Marco <antnyhern@gmail.com>
2021-08-05 23:29:00 -0500

    All proyect

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TuplasFuntionPrograming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = Counten();
+            counter.Increment();
+            counter.SubStract();
+            Console.WriteLine(counter.get());
+        }
+
+        public static (Action Increment, Action SubStract, Func<int> get) Counten()
+        {
+            //Control variable
+            int i = 0;
+            //Actions
+            Action Increment = () => i++;
+            Action SubStract = () => i--;
+            Func<int> get = () => i;
+            //Data return 
+            return (Increment, SubStract, get);
+
+        }
+    }
+}
